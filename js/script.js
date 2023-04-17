ef5a24b524882b6f29394767f99d4826f9f613b4
Collin R <collin.codes.hard@gmail.com>
2023-04-17 13:17:13 -0500

    Created cost calculating function

--- js/script.js
+++ js/script.js
@@ -10,10 +10,23 @@ var subDuration = 1;
 //Create Change Event Listeners
 subTypeElement.addEventListener("change", function(e) {
     subType = e.target.value;
-    console.log(subType);
+   // console.log(subType);
 });
 
 subDurationElement.addEventListener("change", function(e) {
     subDuration = Number(e.target.value);
-    console.log(subDuration);
+   // console.log(subDuration);
 });
+
+//Cost Calculating Function
+ var updateSubscriptionDiv = function () {
+    var monthlyCost = 5;
+    if (subType == "standard"){
+        monthlyCost = 7;
+        if else (subType == "premium") {
+            monthlyCost = 10;
+        }
+    }
+    var total = (subDuration*monthlyCost);
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+ };
