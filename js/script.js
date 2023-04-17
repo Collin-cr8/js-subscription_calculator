fe0c132565f98fb0fa351fc07a77d267b593ee62
Collin R <collin.codes.hard@gmail.com>
2023-04-17 13:32:30 -0500

    Added call function and updated file paths

--- js/script.js
+++ js/script.js
@@ -11,22 +11,23 @@ var subDuration = 1;
 subTypeElement.addEventListener("change", function(e) {
     subType = e.target.value;
    // console.log(subType);
+   updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function(e) {
     subDuration = Number(e.target.value);
    // console.log(subDuration);
+   updateSubscriptionDiv();
 });
 
 //Cost Calculating Function
  var updateSubscriptionDiv = function () {
     var monthlyCost = 5;
-    if (subType == "standard"){
-        monthlyCost = 7;
-        if else (subType == "premium") {
+    if (subType === "standard"){
+        monthlyCost = 7;}
+        else if (subType === "premium") {
             monthlyCost = 10;
         }
-    }
     var total = (subDuration*monthlyCost);
     result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
  };
